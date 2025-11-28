c21a1f7b402fbe6dd3922cbfd0fad9e2efffbf16
Dang Pham <23130042@st.hcmuaf.edu.vn>
2025-11-28 22:41:19 +0700

    Update điều hướng và cách đăng nhập cơ bản của trang login

--- python_project/django_python/urls.py
+++ python_project/django_python/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from library import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('home-user/', views.home_page_user, name='home_page_user'),
+    path('login/', views.custom_login, name='login'),
+    path('login-view/', views.login_view, name='login_view'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
 
