7e4af0da3a025ee635fdfa29cacf218d419def8c
Dang Pham <23130042@st.hcmuaf.edu.vn>
2025-12-11 23:54:20 +0700

    Update template

--- python_project/django_python/settings.py
+++ python_project/django_python/settings.py
@@ -149,9 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'library/static'
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
